510ad0fbe0c34e3111119b08c0a4cd18be17dbbf
Lori Culberson <bigskybandit@sbcglobal.net>
2017-11-10 20:52:27 -0600

    Refactor how i is being set

--- removeDups.js
+++ removeDups.js
@@ -1,20 +1,20 @@
 function removeDups(nums) {
-  i = 0;
+  let i = 0;
   while (i <= nums.length - 2) {
     let currentValue = nums[i];
     let nextValue = nums[i+1];
     if (currentValue === nextValue){
       //delete currentValue and reset i by --1
       nums.splice(i, 1);
-      i--;
-    }
+      //i-=1;
+    } else {
     //console.log('Current: ', currentValue);
     //console.log('Next:', nextValue);
-    i++;
+      i+=1;
+    }
   }
   return nums;
 }
-
 
 function uniqValuesCounter(noDupes) {
   return noDupes.length;
